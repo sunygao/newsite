c69c1f6a45c9be06ec160f68199851021f9d08d2
Suny Gao <suny.gao@gmail.com>
2017-01-25 16:24:27 -0500

    express app

---
+++ app/webpack.config.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  resolve: {
+    root: [
+      path.resolve('src/scripts'),
+      path.resolve('src/scripts/vendor'),
+    ],
+    alias: {
+      'jquery' : 'zepto',
+      'TweenMax' : 'TweenMax',
+      '_' : 'lodash-min'
+    }
+  },
+  entry: [
+    'babel-polyfill',
+    'main.js',
+  ],
+  output: {
+  	path: path.resolve('public/javascripts/'),
+    publicPath: '/javascripts/',
+    filename: 'main.js'
+  },
+  debug: true,
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      { 
+        test: /\.js$/,
+        include: path.join(__dirname, 'src/scripts'),
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: [require.resolve('babel-preset-es2015')],
+          plugins: [
+            ["transform-es2015-classes", {
+              "loose": true
+            }]
+          ]
+        }
+      }
+    ]
+  },
+  devServer: {
+    hot: true,
+    publicPath: '/javascripts/',
+    proxy: {
+      '**': 'http://localhost:3000'
+    }
+  }
+};
