754701e4555256450e0029009760cd2a21a12ea7
Suny Gao <sunygao@paypal.com>
2019-12-09 15:07:06 -0500

    add sidewalk toronto json file

--- data/web-projects/index.js
+++ data/web-projects/index.js
@@ -1,5 +1,5 @@
 //shared between server and fe - use commonjs
-
+var SidewalkToronto = require('./sidewalk-toronto.json')
 var NotableWomen = require('./notable-women.json')
 var Chobani = require('./chobani.json')
 var ISmokeWeed = require('./i-smoke-weed.json')
@@ -7,7 +7,7 @@ var YearInMusic = require('./year-in-music.json')
 var GoogleCalendar = require('./google-calender.json')
 var CampGoogle = require('./camp-google.json')
 
-const allWebObj = Object.assign({}, NotableWomen, Chobani, ISmokeWeed, YearInMusic, GoogleCalendar, CampGoogle); //as an object, for individual work routes
+const allWebObj = Object.assign({}, SidewalkToronto, NotableWomen, Chobani, ISmokeWeed, YearInMusic, GoogleCalendar, CampGoogle); //as an object, for individual work routes
 
 const allWebArray = []; //list of keys, for next data
 
