3e9b04b4d8d8baf715cb9282e453172a1646449d
Suny Gao <sunygao@paypal.com>
2019-12-09 18:22:18 -0500

    adding json for artwork

--- data/art-projects/index.js
+++ data/art-projects/index.js
@@ -1,8 +1,12 @@
-//shared between server and fe - use commonjs
-
 import RovingWoman from './roving-woman.json'
+import RoomForRent from './room-for-rent.json'
+import VenusFlyTraps from './10-venus-fly-traps.json'
+import PrintedMatter from './printed-matter.json'
+import DiaryComics from './diary-comics.json'
+import Phenakistiscopes from './phenakistiscopes.json'
+import IllustrationsVarious from './illustrations-various.json'
 
-const allArtObj = Object.assign({ }, RovingWoman); //as an object, for individual work routes
+const allArtObj = Object.assign({ }, RovingWoman, RoomForRent, DiaryComics, Phenakistiscopes, IllustrationsVarious, VenusFlyTraps, PrintedMatter); //as an object, for individual work routes
 
 const allArtArray = []; //list of keys, for next data
 
