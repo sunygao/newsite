31d90f8ae92cabe764bf0fd88ea3fa0bfae2ba57
Suny Gao <suny.gao@gmail.com>
2021-08-19 20:04:40 -0400

    third eye page

--- data/third-eye.js
+++ data/third-eye.js
@@ -1,4 +1,5 @@
 export default {
 	"title" : "Suny Gao - Third Eye",
-	"description" : "Third Eye, a phenakistiscope"
+	"description" : "Third Eye, a phenakistiscope",
+	"pageClass": "third-eye"
 }
--- routes/thirdEye.js
+++ routes/thirdEye.js
@@ -9,19 +9,24 @@ router.get('/', function(req, res, next) {
 	var manifest = req.app.get('manifest');
 	var env = req.app.get('env');
 
-	var indexPage = pug.compileFile('views/thirdEye.pug');
+
+
+	var thirdEyePage = pug.compileFile('views/thirdEye.pug');
 	var layout = pug.compileFile('views/layout.pug', {
 		filters: {
 			'content': function () {
-			    return indexPage();
+			    return thirdEyePage({ pathname: 'test', data: 'test' });
 			  }
 		}
 	});
 	var html = layout({ 
-		meta: thirdEyeData,
+		title: thirdEyeData.title,
+		description: thirdEyeData.description,
+		pageClass: thirdEyeData.pageClass,
 		env: env,
-		manifest: manifest,
+		manifest: manifest
 	});
+
 	res.send(html);
 });
 
